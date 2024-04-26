c15bf836ba3fd6421837ed8a47d85fc5004654bc
dr0th3r <dr0th3r.code@gmail.com>
2024-04-26 20:00:30 +0200

    feat: add player manager

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,17 +3,21 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class GamePanel extends JPanel implements Runnable {
     public static final int WIDTH = 1280;
     public static final int HEIGHT = 720;
     final int FPS = 60;
     Thread gameThread;
+    PlayManager pm;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.BLACK);
         this.setLayout(null);
+
+        pm = new PlayManager();
     }
 
     public void launchGame() {
@@ -43,10 +47,13 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     private void update() {
-
+        pm.update();
     }
     
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        pm.draw(g2);
     }
 }
